105b8fb5e0bbe2880d9b8911d1ae6a015497f41e
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2022-05-12 22:45:24 +0530

    progress saved

---
+++ src/Tree.jsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
+
+const data = {
+  id: 'root',
+  name: 'Parent',
+  children: [
+    {
+        id: '1',
+        title: 'Node 1',
+        children: [
+          {
+            id: '1.1',
+            title: 'Node 1.1',
+            children: [],
+          },
+          {
+            id: '1.2',
+            title: 'Node 1.2',
+            children: [],
+          },
+        ],  
+    },
+    {
+        id: '2',
+        title: 'Node 2',
+        children: [
+          {
+            id: '2.1',
+            title: 'Node 2.1',
+            children: [],
+          },
+        ], 
+    },
+  ],
+};
+  
+
+export default function Tree() {
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.title} >
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+
+  return (
+    <TreeView
+      aria-label="rich object"
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpanded={['root']}
+      defaultExpandIcon={<ChevronRightIcon />}
+      draggable={true}
+    >
+      {renderTree(data)}
+    </TreeView>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Tree from "./Tree";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<Tree />
+	</React.StrictMode>
+);
